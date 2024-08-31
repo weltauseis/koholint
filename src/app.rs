ca811fffbb1e721753fad16d00ad9e62be62fe3a
Kevin <kevin.jourdain@etu.u-bordeaux.fr>
2024-08-31 23:49:00 +0200

    eframe & egui test

---
+++ src/app.rs
@@ -0,0 +1,60 @@
+pub struct EmulatorApp {}
+
+impl EmulatorApp {
+    /// Called once before the first frame.
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // This is also where you can customize the look and feel of egui using
+        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+
+        return EmulatorApp {};
+    }
+}
+
+impl eframe::App for EmulatorApp {
+    /// Called by the frame work to save state before shutdown.
+    /*  fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    } */
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
+        // For inspiration and more examples, go to https://emilk.github.io/egui
+
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            // The top panel is often a good place for a menu bar:
+
+            egui::menu::bar(ui, |ui| {
+                // NOTE: no File->Quit on web pages!
+                let is_web = cfg!(target_arch = "wasm32");
+                if !is_web {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("Quit").clicked() {
+                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                        }
+                    });
+                    ui.add_space(16.0);
+                }
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // The central panel the region left after adding TopPanel's and SidePanel's
+            ui.heading("eframe template");
+
+            /*  ui.horizontal(|ui| {
+                ui.label("Write something: ");
+                ui.text_edit_singleline(&mut self.label);
+            });
+
+            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
+            if ui.button("Increment").clicked() {
+                self.value += 1.0;
+            } */
+
+            ui.separator();
+
+            ui.label("ALPHA SOFTWARE : DO NOT USE AS A REAL EMULATOR !");
+        });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use winit::{
     application::ApplicationHandler, event::WindowEvent, event_loop::EventLoop, window::Window,
 };
 
+mod app;
 #[allow(dead_code)]
 mod cpu;
 mod debugger;
@@ -16,97 +17,25 @@ mod gameboy;
 #[allow(non_contiguous_range_endpoints)]
 mod memory;
 
-struct Application {
-    window: Option<Window>,
-}
-
-impl ApplicationHandler<EmulatorEvent> for Application {
-    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        info!("Resumed the event loop");
-
-        // Create initial window.
-        self.window = Some(
-            event_loop
-                .create_window(Window::default_attributes())
-                .expect("failed to create initial window"),
-        );
-    }
-
-    fn window_event(
-        &mut self,
-        event_loop: &winit::event_loop::ActiveEventLoop,
-        window_id: winit::window::WindowId,
-        event: winit::event::WindowEvent,
-    ) {
-        match event {
-            WindowEvent::CloseRequested => {
-                println!("The close button was pressed; stopping");
-                event_loop.exit();
-            }
-            WindowEvent::RedrawRequested => {
-                // Redraw the application.
-                //
-                // It's preferable for applications that do not render continuously to render in
-                // this event rather than in AboutToWait, since rendering in here allows
-                // the program to gracefully handle redraws requested by the OS.
-
-                // Draw.
-
-                // Queue a RedrawRequested event.
-                //
-                // You only need to call this if you've determined that you need to redraw in
-                // applications which do not always need to. Applications that redraw continuously
-                // can render here instead.
-                self.window.as_ref().unwrap().request_redraw();
-            }
-            _ => (),
-        }
-    }
-
-    fn user_event(
-        &mut self,
-        _event_loop: &winit::event_loop::ActiveEventLoop,
-        event: EmulatorEvent,
-    ) {
-        info!("User event: {event:?}");
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-enum EmulatorEvent {
-    VRAMUpdate,
-}
-
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
         println!("Usage : gbemulator <rom file>");
         return;
     }
-
     env_logger::init();
 
-    /* loop {
-        gameboy.step();
-    } */
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0]),
+        ..Default::default()
+    };
 
-    let event_loop = EventLoop::<EmulatorEvent>::with_user_event()
-        .build()
-        .unwrap();
-    let event_loop_proxy = event_loop.create_proxy();
-
-    std::thread::spawn(move || {
-        let rom = std::fs::read(&args[1]).unwrap();
-        let gameboy = Gameboy::new(rom);
-
-        /* loop {
-            let _ = event_loop_proxy.send_event(EmulatorEvent::VRAMUpdate);
-            std::thread::sleep(std::time::Duration::from_secs(1));
-        } */
-
-        debug_console(gameboy);
-    });
-
-    let mut app = Application { window: None };
-    event_loop.run_app(&mut app).unwrap();
+    eframe::run_native(
+        "Koholint Gameboy Emulator",
+        native_options,
+        Box::new(|cc| Ok(Box::new(app::EmulatorApp::new(cc)))),
+    )
+    .unwrap();
 }
