16a199872f6adfb6370c409e1cf8d3a71bb40601
Kevin <kevin.jourdain@etu.u-bordeaux.fr>
2024-09-03 18:15:34 +0200

    wgpu example

--- src/debugger.rs
+++ src/debugger.rs
@@ -7,18 +7,15 @@ use log::error;
 
 use crate::{decoding::decode_instruction, gameboy::Gameboy};
 
-pub fn debug_console(console: Arc<Mutex<Gameboy>>) {
+pub fn debug_console(mut console: Gameboy) {
     println!("Welcome to my GBC debugger !");
 
     let mut breakpoints: Vec<u16> = Vec::new();
 
     loop {
         // prompt
-        {
-            let console = console.lock().unwrap();
-            print!("{:#06X} (dbg)> ", console.cpu().read_program_counter());
-            std::io::stdout().flush().unwrap();
-        }
+        print!("{:#06X} (dbg)> ", console.cpu().read_program_counter());
+        std::io::stdout().flush().unwrap();
 
         // get user input
         let mut input = String::new();
@@ -33,7 +30,6 @@ pub fn debug_console(console: Arc<Mutex<Gameboy>>) {
         match subcommands.get(0) {
             None => {
                 // step program if empty command
-                let mut console = console.lock().unwrap();
                 console.step();
             }
             Some(cmd) => {
@@ -57,8 +53,6 @@ pub fn debug_console(console: Arc<Mutex<Gameboy>>) {
                     }
                     "list" | "l" => {
                         // TODO: find a way to show previous instructions
-                        let console = console.lock().unwrap();
-
                         let pc = console.cpu().read_program_counter();
                         let mut to_list = match subcommands.get(1) {
                             None => 5,
@@ -90,30 +84,26 @@ pub fn debug_console(console: Arc<Mutex<Gameboy>>) {
                             println!("Error : Missing register or flag name");
                             continue;
                         }
-                        Some(reg_name) => {
-                            let console = console.lock().unwrap();
-                            match *reg_name {
-                                "a" => println!("a : {:#04X}", console.cpu().read_a_register()),
-                                "b" => println!("b : {:#04X}", console.cpu().read_b_register()),
-                                "c" => println!("c : {:#04X}", console.cpu().read_c_register()),
-                                "d" => println!("d : {:#04X}", console.cpu().read_d_register()),
-                                "e" => println!("e : {:#04X}", console.cpu().read_e_register()),
-                                "h" => println!("h : {:#04X}", console.cpu().read_h_register()),
-                                "l" => println!("l : {:#04X}", console.cpu().read_l_register()),
-                                "bc" => println!("bc : {:#06X}", console.cpu().read_bc_register()),
-                                "de" => println!("de : {:#06X}", console.cpu().read_de_register()),
-                                "hl" => println!("hl : {:#06X}", console.cpu().read_hl_register()),
-                                "sp" => {
-                                    println!("sp : {:#06X}", console.cpu().read_stack_pointer())
-                                }
-                                _ => {
-                                    println!("Error : Unknown register or flag ({})", reg_name);
-                                }
+                        Some(reg_name) => match *reg_name {
+                            "a" => println!("a : {:#04X}", console.cpu().read_a_register()),
+                            "b" => println!("b : {:#04X}", console.cpu().read_b_register()),
+                            "c" => println!("c : {:#04X}", console.cpu().read_c_register()),
+                            "d" => println!("d : {:#04X}", console.cpu().read_d_register()),
+                            "e" => println!("e : {:#04X}", console.cpu().read_e_register()),
+                            "h" => println!("h : {:#04X}", console.cpu().read_h_register()),
+                            "l" => println!("l : {:#04X}", console.cpu().read_l_register()),
+                            "bc" => println!("bc : {:#06X}", console.cpu().read_bc_register()),
+                            "de" => println!("de : {:#06X}", console.cpu().read_de_register()),
+                            "hl" => println!("hl : {:#06X}", console.cpu().read_hl_register()),
+                            "sp" => {
+                                println!("sp : {:#06X}", console.cpu().read_stack_pointer())
                             }
-                        }
+                            _ => {
+                                println!("Error : Unknown register or flag ({})", reg_name);
+                            }
+                        },
                     },
                     "flags" | "f" => {
-                        let console = console.lock().unwrap();
                         println!(
                             "{} {} {} {}",
                             console.cpu().read_z_flag() as u8,
@@ -123,11 +113,9 @@ pub fn debug_console(console: Arc<Mutex<Gameboy>>) {
                         );
                     }
                     "next" | "n" => {
-                        let mut console = console.lock().unwrap();
                         console.step();
                     }
                     "continue" | "c" => loop {
-                        let mut console = console.lock().unwrap();
                         console.step();
 
                         let pc = console.cpu().read_program_counter();
@@ -180,8 +168,6 @@ pub fn debug_console(console: Arc<Mutex<Gameboy>>) {
                         }
                     },
                     "dump" => {
-                        let console = console.lock().unwrap();
-
                         const WIDTH: u16 = 16; // in tiles
                         const HEIGHT: u16 = 24; // in tiles, should be equal to 384/WIDTH
 
--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ fn main() {
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
+            .with_inner_size([512.0, 512.0])
             .with_min_inner_size([300.0, 220.0]),
         ..Default::default()
     };
